d8f1f0c31de209ca9229212cfdf887ef3e3913fd
da cunha enzo <dcn3386a@ens.fsi>
2022-09-26 19:55:15 +0200

    test

---
+++ src/personnages/Village.java
@@ -0,0 +1,17 @@
+package personnages;
+
+public class Village {
+	private String nom;
+
+	public Village(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	
+	
+}
